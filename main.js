ae8298b9902270271138d0c068a44245b48c195c
Fate <fate.public@gmail.com>
2023-10-10 21:21:29 +0500

    Can set colors using input form and clear playground

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 * @type HTMLCanvasElement
 */
 
+let pixelColor = 'tomato';
+const colorForm = document.getElementById('form');
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const clearButton = document.getElementById('clear');
@@ -13,7 +15,7 @@ ctx.fillStyle = "#333333";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 
-const PixelCount = 5;
+const PixelCount = 20;
 const pixelSize = canvas.width / PixelCount;
 
 function handleMouseDown(e) {
@@ -25,17 +27,24 @@ function handleMouseDown(e) {
     drawPixel(startX, startY);
 }
 
+function clear() {
+    ctx.fillStyle = "#333333";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
 function drawPixel(x, y) {
     const 
         startX = x * pixelSize,
         startY = y * pixelSize
-    ctx.rect(startX, startY, pixelSize, pixelSize);
-    ctx.fillStyle = "tomato";
-    ctx.fill();
+    
+    ctx.fillStyle = pixelColor;
+    ctx.fillRect(startX, startY, pixelSize, pixelSize);
 }
 
 canvas.addEventListener('mousedown', handleMouseDown);
-clearButton.addEventListener('click', () => {
-    ctx.fillStyle = "#333333";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+clearButton.addEventListener('click', clear);
+colorForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    pixelColor = `#${document.getElementById('formInput').value}`
 })
